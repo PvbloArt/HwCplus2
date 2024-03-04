386a7800d30055d832c95cfdb7f55a434406e9da
Artyom Pvblo <sotr.pvblo@mail.ru>
2024-03-04 20:56:19 +0300

    Добавьте файлы проекта.

---
+++ HwCplus2.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <string>
+
+int main() {
+	std::string message = "Hello skillbox)))!That Pvlblo //pls help";
+
+	std::cout << message << std::endl;
+	std::cout << "line length: " << message.length() << std::endl;
+	std::cout << "first symbol: " << message.front() << std::endl;
+	std::cout << "last symbol: " << message.back() << std::endl;
+
+	return 0;
+}
